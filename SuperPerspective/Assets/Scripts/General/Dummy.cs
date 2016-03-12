6e3e47477e9f11848e642b91ed75c51e0e8e38e8
ArendPeter <apc1993@gmail.com>
2016-03-11 20:59:57 -0500

    Worked on framerate debugging

---
+++ SuperPerspective/Assets/Scripts/General/Dummy.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dummy : MonoBehaviour {
+	public void Destroy(){
+		Destroy();
+	}
+}
---
+++ SuperPerspective/Assets/Scripts/General/Lagger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lagger : MonoBehaviour {
+	public int iterations;
+	public GameObject prefab;
+
+	void Update () {
+		foreach(Dummy obj in GameObject.FindObjectsOfType<Dummy>()){
+			GameObject.Destroy(obj.gameObject)	;
+		}
+		for(int i = 0; i < iterations; i++){
+			Instantiate(prefab);
+		}
+	}
+
+
+}
