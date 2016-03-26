6188562d47bf01a29408bb5c3e36b07db2a548a5
Larry M. Smith <lsmith76@gatech.edu>
2016-03-26 01:59:45 -0400

    Text Testing Scene
    SO. As discussed with Shooter, I added in my fully realized RPG text
    readout system. Right now is just a proof of concept, since I don't know
    nearly enough about ANY of our components to even TRY ghetto-rigging it
    into Hoodie's params.  As of right now, I have "ShowBox" checked, which
    makes it read out text from a ConvoNode class automatically.  Just call
    "StartConvo(convoNode myConvoNode)" on the TextBoxScript, and it should
    read out whatever ConvoNode you give it.
    
    ConvoNodes also have the capabilities to answer Questions and such, but
    if you never set the Convo Node type to Question or Both, we won't have
    to bother with dummying this feature out. Using this, we can make little
    "Hint Wisps" to talk to during gameplay.
    
    Just look in the GrassScene+Text.scene I left out. for an example. (we
    can delete this scene later.)

---
+++ SuperPerspective/Assets/Scripts/TextBox Scripts/askBoxScript.cs
@@ -0,0 +1,275 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class askBoxScript : MonoBehaviour {
+
+	//Visualization variables
+	public GameObject textStartPoint; //Tells where our text will begin generating.
+    public convoNode currentNode; //This is the node we are currently pulling info from
+    public textBoxScript textBoxPartner; //Our askBox's textbox partner to switch between
+    //public playerChar player;
+
+    public string question;//The question we ponder the the answer to.
+	public string[] choiceArray; //Holds the strings we want to display in the textbox.
+
+   private char[] charArray; //Holds our strings, converted to characters
+
+	public int lineLength = 25; //Number of characters per line
+	public float kerning = 1f;//Space between characters
+	public float vSpace = 6f;//Space between lines
+
+	//Display Speed variables
+	public float textSpeed = 15f;
+	public float endLineBlinkSpeed = 1f;
+	private float resetTextSpeed;
+	private float tinyTimer = 1f;
+	private float resetTimer;
+
+	//Keeps track of our cursor so we know where we are in the string.
+	private int horiIndex = 0;
+	private int vertIndex = 0;
+	private int progressIndex = 0;//We use this to check which question we're currently drawing out
+
+	public int choiceID = 0;//How we publically track what choice we're making. This controls where the cursor will be as well.
+
+	[SerializeField] private Text[] textArray;//An array of Text objects spaced evenly. Will eventually be filled with a dynamic amount of choices for us to choose from.
+
+	//Determines whether or not we should show the blinking line ending.
+	private bool showEndLine = false;
+	//Determines whether or not we have finished displaying the line
+	private bool isFinishedDisplaying = false;
+	//Determine whether or not we should be able to see the textbox at all.
+	public bool showBox = false;
+	//The textbox we want to edit.
+	public UnityEngine.UI.Image textbox;
+	
+	public Text text;//Text that we use to display our cursor
+    public Text qText;//Text which we use to display our actual QUESTION
+
+	// Use this for initialization
+	void Start () {
+		resetTimer = tinyTimer;
+		resetTextSpeed = textSpeed;
+		init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		showTextboxCheck();
+		printLoop();
+		interactLoop();
+	}
+
+	public void init()
+	{
+        //player = (playerChar)GameObject.FindObjectOfType(typeof(playerChar));//DYNAMICALLY GET OUR CHARACTER ON INIT!
+        if (currentNode.myType != convoNode.nodeType.textOnly)
+        { 
+            progressIndex = 0;
+            choiceID = 0;
+		    text.text = "";
+            qText.text = currentNode.question;
+            choiceArray = currentNode.endQuestionArray;
+		    tinyTimer = resetTimer;
+		    textSpeed = resetTextSpeed;
+		    charArray = new char[choiceArray[progressIndex].Length];//We get the length of the string in our current conversationPoint.
+		    isFinishedDisplaying = false;
+		    //prepStrings();
+		    horiIndex = 0;
+		    vertIndex = 0;
+
+		    textArray = new Text[choiceArray.Length];
+            charArray = choiceArray[progressIndex].ToCharArray();
+
+            //Our basic Text is used as a cursor. The rest of the texts are stored in the textArray
+
+            for (int i = 0; i < choiceArray.Length; i++)//Here we instantiate ALL of the new text objects
+		    {
+			    Text temp = (Text)Instantiate(text, textStartPoint.transform.position, transform.rotation);
+			    //temp.text = "TEST";
+			    temp.transform.SetParent(textStartPoint.transform, false);
+			    temp.alignment = TextAnchor.MiddleLeft;
+			    temp.transform.localPosition = new Vector3(0, 0 - (vSpace * i), 0);
+			    textArray[i] = temp;
+		    }
+        }
+    }
+
+    //Dynamic function to determine whether or not the askbox should be showing based on a variable.
+	public void showTextboxCheck()
+	{
+		if (!showBox)
+		{
+			disableBox();
+		}
+		else
+		{
+			enableBox();
+		}
+	}
+
+	public void enableBox()//This enables the textbox so you can see it. It also resets the textbox.
+	{
+        
+		textbox.enabled = true;
+		text.enabled = true;
+        qText.enabled = true;
+
+        for (int i = 0; i < choiceArray.Length; i++)//Here we instantiate ALL of the new text objects
+        {
+            textArray[i].enabled = true;
+        }
+    }
+
+	public void disableBox()//This disables the textbox so that you can't see it.
+	{
+		textbox.enabled = false;
+		text.enabled = false;
+        qText.enabled = false;
+
+        deleteText();
+    }
+
+    public void activate(convoNode q)
+    {
+        currentNode = q;
+        question = currentNode.question;
+        choiceArray = q.endQuestionArray;
+        init();
+        enableBox();
+        showBox = true;
+    }
+
+	//This method acts as an update loop for when the text box should be displaying normal conversation text.
+	public void printLoop()
+	{
+		if (showBox)
+		{//If we're even active...
+			tinyTimer -= Time.deltaTime * textSpeed; //Run the timer.
+
+			//Every time we time down to zero, do the thing.
+			if (tinyTimer <= 0)
+			{
+				if (progressIndex < choiceArray.Length)//This allows us to increment our texts properly.
+				{
+					if (horiIndex < charArray.Length)//If we haven't filled out to the end of our singular line...
+					{
+						charArray = choiceArray[progressIndex].ToCharArray();
+						textSpeed = resetTextSpeed;
+						//Create a character at the horizontal length
+						textArray[progressIndex].text = textArray[progressIndex].text + charArray[horiIndex];//This controls which actual TEXT object we're writing to.
+						horiIndex++;
+						if (horiIndex % lineLength == 0 && horiIndex != charArray.Length)//Check if we've hit the line length by checking if your horiIndex is divisible by our linelength
+						{
+							switch (charArray[horiIndex])//Special cases for line breaks if we hit the line length with our one-line responses....
+							{
+
+								case ' ':
+									text.text = text.text + "\n"; //We've hit an end, NEXT LINE.
+									vertIndex++;//Move down a line if we've hit the end.
+									break;
+								default:
+									text.text = text.text + "-\n"; //We've hit an end, NEXT LINE.
+									vertIndex++;//Move down a line if we've hit the end.
+									break;
+							}
+						}
+						else if(horiIndex >= charArray.Length)//If we've finished printing out one choice, reset and go to the next choice.
+						{
+							horiIndex = 0;
+							vertIndex = 0;
+							progressIndex++;
+						}
+					}
+				}
+				else//We only arrive here AFTER we've printed out all of our options.
+				{
+					isFinishedDisplaying = true;
+				}
+
+				tinyTimer = resetTimer;
+			}
+		}
+	}
+
+	public void makeChoice()
+	{
+		isFinishedDisplaying = false;
+        //First we check if what our answer even corresponds to even exists.
+        if (currentNode.hasNext && currentNode.nextNodeArray[choiceID] != null)
+        {
+            switch (currentNode.myType)
+            {
+
+                case convoNode.nodeType.textOnly://Switch to Text Mode
+                    textBoxPartner.startConvo(currentNode.nextNodeArray[choiceID]);
+                    disableBox();
+                    break;
+                case convoNode.nodeType.both://Switch to Text Mode
+                    textBoxPartner.startConvo(currentNode.nextNodeArray[choiceID]);
+                    disableBox();
+                    break;
+
+                case convoNode.nodeType.question://Load up and Re-init!
+                    currentNode = currentNode.nextNodeArray[choiceID];
+                    init();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        else {
+            print("disableBox");
+            disableBox();
+            showBox = false;
+        }
+	}
+
+	public void deleteText(){
+
+        if (!(textArray == null))
+        {
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                Destroy(textArray[i].gameObject);
+            }
+            textArray = null;
+        }
+	}
+
+	public void interactLoop()
+	{
+		if (isFinishedDisplaying) { //If it's finshed displaying, that means it's time to MAKE A CHOICE.
+			text.alignment = TextAnchor.MiddleRight;
+			text.transform.position = new Vector3(textArray[choiceID].transform.position.x - 1, textArray[choiceID].transform.position.y, 0);
+			text.text = "○";
+
+			if (Input.GetKeyUp(KeyCode.Space))
+			{
+				makeChoice();
+			}
+
+			if (Input.GetKeyUp(KeyCode.UpArrow) && choiceID != 0)
+			{
+				choiceID--;
+			}
+			if (Input.GetKeyUp(KeyCode.DownArrow) && choiceID < choiceArray.Length-1)
+			{
+				choiceID++;
+			}
+		}
+	}
+
+    public void prepStrings()
+    {
+
+            //Convert all of the strings of our current conversationPoint to characters
+            charArray = choiceArray[choiceID].ToCharArray();
+            horiIndex = 0;
+            vertIndex = 0;
+
+    }
+
+}
---
+++ SuperPerspective/Assets/Scripts/TextBox Scripts/textBoxScript.cs
@@ -0,0 +1,348 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class textBoxScript : MonoBehaviour {
+
+
+
+//Visualization variables
+	public GameObject textStartPoint; //Tells where our text will begin generating.
+
+    public convoNode currentNode; //This is the node we are currently pulling info from
+
+    public askBoxScript askBoxPartner; //Our textBox's askBox partner to switch between
+    //public playerChar player;//Our connection to the player in the scene.
+
+
+    public string[] convArray; //Holds the strings we want to display in the textbox.
+	private char[] charArray; //Holds our strings, converted to characters
+	private int conversationPoint = 0;//Tells us how many nodes into the conversation we are.
+
+	public int lineLength = 25; //Number of characters per line
+	public float kerning = 1f;//Space between characters
+	public float vSpace = 1f;//Space between lines
+
+//Display Speed variables
+	public float textSpeed = 15f;
+	public float endLineBlinkSpeed = 1f;
+	private float resetTextSpeed;
+	private float tinyTimer = 1f;
+	private float resetTimer;
+
+//Keeps track of our cursor so we know where we are in the string.
+	private int horiIndex = 0;
+	private int vertIndex = 0;
+
+	//Branching option variables
+	public string[] choiceArray; // The strings that make up the answers we can make
+	
+
+	public int questionLine = 0; //Used to track which line we are currently making.
+	public int choiceIndex = 0;//Used for indicating what choice we're thinking of making.
+
+//Determines whether or not we should show the blinking line ending.
+	private bool showEndLine = false;
+//Determines whether or not we have finished displaying the line
+	private bool isFinishedDisplaying = false;
+//Determine whether or not we should be able to see the textbox at all.
+	public bool showBox = false;
+//The textbox we want to edit.
+	public UnityEngine.UI.Image textbox;
+//Text so we have something to edit
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		showTextboxCheck();
+		printLoop();
+
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			progressConv();
+		}
+
+	}
+
+    //The initialization function for when our program has started running.
+    public void init()
+    {
+        //player = (playerChar)GameObject.FindObjectOfType(typeof(playerChar));//DYNAMICALLY GET OUR CHARACTER ON INIT!
+        if (currentNode.myType != convoNode.nodeType.question) {//Make sure to check whether or not we should even be bothering to set up.
+            convArray = currentNode.convoTextArray;//Load up our text to sidplay from our node.
+            text.text = "";//Make sure our text is empty before we start.
+
+            //We set these next two so that the timers will know what number to reset to.
+            resetTimer = tinyTimer;
+            resetTextSpeed = textSpeed;
+
+            charArray = new char[convArray[conversationPoint].Length];//We get the length of the string in our current conversationPoint.
+            isFinishedDisplaying = false;
+            conversationPoint = 0;//So that we start at the BEGINNING of the convo
+            prepStrings();
+        }
+        else//If it's just a question, immediately activate our Ask Box buddy.
+        {
+            disableBox();
+            //askQuestion(currentNode);
+        }
+	}
+
+	//Initialize after the program has started
+	public void reInit()
+	{
+		text.text = "";
+		tinyTimer = resetTimer;
+		textSpeed = resetTextSpeed;
+		conversationPoint = 0;
+		charArray = new char[convArray[conversationPoint].Length];//We get the length of the string in our current conversationPoint.
+		isFinishedDisplaying = false;
+		prepStrings();
+		horiIndex = 0;
+		vertIndex = 0;
+	}
+
+	//Makes the box show after resetting it.
+	public void startConvo()
+	{
+
+        if (askBoxPartner.showBox == false)
+        {
+            if (currentNode.myType != convoNode.nodeType.question)
+            {
+                showBox = true;
+                textbox.enabled = true;
+                text.enabled = true;
+                init();
+            }
+            else
+            {
+                askQuestion(currentNode);
+            }
+        }
+	}
+
+
+	//Overloaded enable that allows for new conversations to be loaded in.
+	public void startConvo(convoNode newNode)
+	{
+
+        if (askBoxPartner.showBox == false)
+        {
+            if (currentNode.myType == convoNode.nodeType.question)
+            {
+                askQuestion(currentNode);
+
+            }
+            else
+            {
+
+                currentNode = newNode;
+                showBox = true;
+                convArray = currentNode.convoTextArray;
+                init();
+                enableBox();
+            }
+        }
+	}
+
+
+	public void endConvo()//Ends the conversation! Use this at the end of EVERYTHING.
+	{
+		showBox = false;
+		disableBox();
+	}
+	public void enableBox()//This enables the textbox so you can see it.
+	{
+		textbox.enabled = true;
+		text.enabled = true;
+        showBox = true;
+	}
+
+	public void disableBox()//This disables the textbox so that you can't see it.
+	{
+		textbox.enabled = false;
+		text.enabled = false;
+        showBox = false;
+	}
+
+	//Resets the Text so that the next line can display properly.
+	public void resetDisplay()
+	{
+		tinyTimer = resetTimer;
+		textSpeed = resetTextSpeed;
+		prepStrings();
+		text.text = "";
+		showEndLine = false;
+		isFinishedDisplaying = false;
+		conversationPoint++;
+		charArray = new char[convArray[conversationPoint].Length];//We get the length of the string in our current conversationPoint.
+		prepStrings();
+	}
+
+    //This runs constantly to decide whether or not the graphics should be displayed or not.
+	public void showTextboxCheck()
+	{
+		if (!showBox || askBoxPartner.showBox)
+		{
+			disableBox();
+		}
+		else if (showBox)
+		{
+			enableBox();
+		}
+	}
+
+	//Go 1 step forward in the conversation
+	public void progressConv()
+	{
+		if (showBox) {//If we're actually active....
+
+            /*
+            Okay, so if we're actually active, at this point we wanna check what type of node we're handling.
+            At this point, there's two cases. Either [it has text] or [it has no text]
+                Case 1 happens when we're either [both] or [textOnly]
+                Case 2 happens when we're [question]
+            */
+
+            //Case 1. WE DEFINITELY HAVE TEXT. We'll check if there's a question at the end later.
+            if (currentNode.myType == convoNode.nodeType.textOnly || currentNode.myType == convoNode.nodeType.both)
+            {
+                if (horiIndex == charArray.Length)
+                { //If our line is finished displaying...
+                    if (conversationPoint < convArray.Length - 1)
+                    {//...And we aren't at the end of our conversation, then do this.
+                        resetDisplay();//This moves us to the next text display.
+
+                    }
+                    else//If we ARE at the end of our conversation, close the box. OR move to the question at the end.
+                    {
+                        if (currentNode.myType == convoNode.nodeType.both) //THIS MEANS WE HAVE A QUESTION TO ANSWER. Activate the Askbox.
+                        {
+                            askQuestion(currentNode);
+                        }
+                        else //This means we have no questions, and can safely end the conversation once and for all.
+                        {
+                            endConvo();
+                        }
+                    }
+                }
+                else//Otherwise, MAKE THE LINE FINISH DISPLAYING.
+                {
+                    displaySpeedSkip();
+                }
+            }
+            else//This means the currentNode's type is "question" and we can turn on the AskBox.
+            {
+                askQuestion(currentNode);
+            }
+
+
+        }
+	}
+
+    //If the player is impatient and wants the text to display instantly isntead of being shown one character at a time, use this to force the whole line to display at once.
+    public void displaySpeedSkip()
+    {
+        while (horiIndex < charArray.Length)
+        {
+            text.text = text.text + charArray[horiIndex];
+            horiIndex++;
+            if (horiIndex % lineLength == 0 && horiIndex != charArray.Length)//Check if we've hit the line length by checking if your horiIndex is divisible by our linelength
+            {
+                switch (charArray[horiIndex])
+                {
+
+                    case ' ':
+                        text.text = text.text + "\n"; //We've hit an end, NEXT LINE.
+                        vertIndex++;//Move down a line if we've hit the end.
+                        break;
+                    default:
+                        text.text = text.text + "-\n"; //We've hit an end, NEXT LINE.
+                        vertIndex++;//Move down a line if we've hit the end.
+                        break;
+                }
+            }
+        }
+    }
+
+	public void askQuestion(convoNode q)
+	{
+
+        askBoxPartner.activate(q);
+        disableBox();
+	}
+
+
+	public void prepStrings()
+	{
+		if (conversationPoint < convArray.Length)
+		{
+			//Convert all of the strings of our current conversationPoint to characters
+			charArray = convArray[conversationPoint].ToCharArray();
+			horiIndex = 0;
+			vertIndex = 0;
+		}
+	}
+
+	//This method acts as an update loop for when the text box should be displaying normal conversation text.
+	public void printLoop()
+	{
+		if (showBox) {//If we're even active...
+            
+			tinyTimer -= Time.deltaTime * textSpeed;
+
+			//Every time we time down to zero, do the thing.
+			if (tinyTimer <= 0)
+			{
+
+				if (horiIndex < charArray.Length)
+				{
+					textSpeed = resetTextSpeed;
+					//Create a character at the horizontal length
+					text.text = text.text + charArray[horiIndex];
+					horiIndex++;
+					if (horiIndex % lineLength == 0 && horiIndex != charArray.Length)//Check if we've hit the line length by checking if your horiIndex is divisible by our linelength
+					{
+						switch (charArray[horiIndex])
+						{
+
+							case ' ':
+								text.text = text.text + "\n"; //We've hit an end, NEXT LINE.
+								vertIndex++;//Move down a line if we've hit the end.
+								break;
+							default:
+								text.text = text.text + "-\n"; //We've hit an end, NEXT LINE.
+								vertIndex++;//Move down a line if we've hit the end.
+								break;
+						}
+					}
+					tinyTimer = resetTimer;
+				}
+
+				else
+				{
+					isFinishedDisplaying = true;
+					textSpeed = endLineBlinkSpeed;
+					showEndLine = !showEndLine;
+					if (showEndLine)
+					{
+						text.text = text.text + " ■";
+					}
+					else
+					{
+						text.text = text.text.Remove(text.text.Length - 2);
+					}
+					tinyTimer = resetTimer;
+
+				}
+			}
+		}
+	}
+
+}
