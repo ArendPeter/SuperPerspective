8f0c79cd98d3cc39f395bfcd83b062dcb3bce73e
ArendPeter <apc1993@gmail.com>
2016-04-11 21:24:08 -0400

    Created basic menu

---
+++ SuperPerspective/Assets/Scripts/General/CameraRotate.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour {
+
+	public float test;
+
+	public Material[] skys;
+
+	public float[] trans;
+	public float transitionRange;
+
+	public float rotateSpeed, radius, height;
+
+	private float angle = 0;
+
+	void Start(){
+		RenderSettings.skybox = skys[0];
+	}
+
+	void Update () {
+		updateRotation();
+		updateSkybox();
+	}
+
+	private void updateRotation(){
+		//position
+		angle +=	rotateSpeed * Time.deltaTime;
+		angle %= 2 * Mathf.PI;
+		Vector3 pos = new Vector3(
+			radius * Mathf.Cos(angle),
+			height,
+			radius * Mathf.Sin(angle));
+		transform.position = pos;
+
+		//rotation
+		transform.rotation = Quaternion.LookRotation(-transform.position, Vector3.up );
+	}
+
+	private void updateSkybox(){
+		int curIndex = 0, nextIndex;
+		while(curIndex < 3 && angle > trans[curIndex]){
+			curIndex++;
+		}
+		curIndex = curIndex%3;
+		nextIndex = (curIndex+1)%3;
+		float t = 0;
+		float distToTrans = trans[nextIndex] - angle;
+		if(distToTrans < 0){ distToTrans += Mathf.PI * 2; }
+		if(distToTrans < transitionRange){
+			t = distToTrans / transitionRange;
+		}
+		//skys[3].Lerp(skys[0],skys[1],test);
+		RenderSettings.skybox = skys[curIndex];
+	}
+}
