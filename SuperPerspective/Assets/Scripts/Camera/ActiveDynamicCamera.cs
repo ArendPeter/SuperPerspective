1464a398948567b060e3d3204c960c8759fcecf9
Peter <paytar1000@gmail.com>
2016-05-15 21:33:55 -0400

    Added public letterbox speed variable to ActiveDynamicCamera

--- SuperPerspective/Assets/Scripts/Camera/ActiveDynamicCamera.cs
+++ SuperPerspective/Assets/Scripts/Camera/ActiveDynamicCamera.cs
@@ -7,7 +7,8 @@ public class ActiveDynamicCamera : Activatable {
 	public bool panOnDeactivate = false;
 	public bool deactivateOnReset = false;
 
-	public Texture letterBoxTexture;
+	public Texture letterboxTexture;
+	public float letterboxSpeed = 0.2f;
 
 	public float panDuration = 3;
 
@@ -32,9 +33,9 @@ public class ActiveDynamicCamera : Activatable {
 
 	void OnGUI() {
 		if (panned && letterboxParam < Mathf.PI / 4) {
-			letterboxParam += Mathf.PI / 8 * Time.deltaTime;
+			letterboxParam += Mathf.PI * letterboxSpeed * Time.deltaTime;
 		} else if (!panned && letterboxParam > 0) {
-			letterboxParam -= Mathf.PI / 8 * Time.deltaTime;
+			letterboxParam -= Mathf.PI * letterboxSpeed * Time.deltaTime;
 		}
 		if (letterboxParam < 0)
 			letterboxParam = 0;
@@ -42,8 +43,8 @@ public class ActiveDynamicCamera : Activatable {
 			letterboxParam = Mathf.PI / 4;
 		if (letterboxParam > 0) {
 			GUI.enabled = true;
-			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height / 5.0f * Mathf.Sin(letterboxParam)), letterBoxTexture);
-			GUI.DrawTexture(new Rect(0, Screen.height - (Screen.height / 5.0f * Mathf.Sin(letterboxParam)), Screen.width, Screen.height / 5.0f), letterBoxTexture);
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height / 5.0f * Mathf.Sin(letterboxParam)), letterboxTexture);
+			GUI.DrawTexture(new Rect(0, Screen.height - (Screen.height / 5.0f * Mathf.Sin(letterboxParam)), Screen.width, Screen.height / 5.0f), letterboxTexture);
 		}
 	}
 
