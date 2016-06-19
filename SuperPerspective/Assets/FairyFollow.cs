771399e55cc004b70bffb1481a6be2e9af5afdfd
Spex130 <Gamerguy56@bellsouth.net>
2016-06-18 22:37:14 -0400

    FairyFollow height checks + GrassScene bridge fix.
    FairyFollow now looks for a SpiritMount class (which is embedded in
    Hoodie's tree), so it can find Hoodie independent of whatever scene it's
    in.
    
    Also, the collisions on one of the bridges in GrassScene was off, so I
    fixed it.

--- SuperPerspective/Assets/FairyFollow.cs
+++ SuperPerspective/Assets/FairyFollow.cs
@@ -9,19 +9,70 @@ public class FairyFollow : MonoBehaviour {
     public float floatHeight = 1f;
     public int arrayID = 0;
 
-    public GameObject[] floatPoints = new GameObject[3];
+    public float rayLength = 10f;
+
+    public SpiritMount mount;
+
+    private GameObject[] floatPoints = new GameObject[3];
+    [SerializeField] private bool[] boolArr = new bool[3];
 
 	// Use this for initialization
 	void Start () {
-        //playerC = GameObject.FindObjectOfType<PlayerController>();
-        player = GameObject.Find("HoodieAnim1");
+        init();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 playerPos = new Vector3(floatPoints[arrayID].transform.position.x, floatPoints[arrayID].transform.position.y + floatHeight, floatPoints[arrayID].transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, playerPos, followSpeed);
-        Vector3 vec = Vector3.Lerp(transform.localRotation.eulerAngles, player.transform.localRotation.eulerAngles, followSpeed);
-        transform.rotation = Quaternion.Euler(vec.x, vec.y, vec.z);
+        if (mount != null)
+        {
+            posCheck();
+            Vector3 playerPos = new Vector3(floatPoints[arrayID].transform.position.x, floatPoints[arrayID].transform.position.y + floatHeight, floatPoints[arrayID].transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, playerPos, followSpeed);
+            Vector3 vec = Vector3.Lerp(transform.localRotation.eulerAngles, player.transform.localRotation.eulerAngles, followSpeed);
+            transform.rotation = Quaternion.Euler(vec.x, vec.y, vec.z);
+        }
+        else
+        {
+            init();
+            mount = GameObject.FindObjectOfType<SpiritMount>();
+        }
     }
+
+    public void posCheck()
+    {
+        for(int i = 0; i < floatPoints.Length; i++)
+        {
+            Vector3 up = transform.TransformDirection(Vector3.up) * rayLength;
+            boolArr[i] = Physics.Raycast(floatPoints[i].transform.position, Vector3.up, rayLength);
+            Debug.DrawRay(floatPoints[i].transform.position, up, Color.green);
+        }
+
+        if (boolArr[0])
+        {
+            if(Vector3.Distance(floatPoints[1].transform.position, Camera.main.transform.position) < Vector3.Distance(floatPoints[2].transform.position, Camera.main.transform.position))
+            {
+                arrayID = 1;
+            }
+            else
+            {
+                arrayID = 2;
+            }
+        }
+        else
+        {
+            arrayID = 0;
+        }
+
+    }
+
+    public void init()
+    {
+        //playerC = GameObject.FindObjectOfType<PlayerController>();
+        mount = GameObject.FindObjectOfType<SpiritMount>();
+        floatPoints[0] = mount.topFloatPoint;
+        floatPoints[1] = mount.leftFloatPoint;
+        floatPoints[2] = mount.rightFloatPoint;
+        player = GameObject.Find("HoodieAnim1");
+    }
+
 }
---
+++ SuperPerspective/Assets/SpiritMount.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiritMount : MonoBehaviour {
+
+    public GameObject topFloatPoint;
+    public GameObject leftFloatPoint;
+    public GameObject rightFloatPoint;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
