034f69df9101116942747ff5fc73af151116a09a
ArendPeter <apc1993@gmail.com>
2016-10-03 23:14:59 -0400

    Added menu transition

---
+++ SuperPerspective/Assets/Scripts/General/SceneChanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+
+	void FixedUpdate(){
+		if(Input.anyKey){
+			Application.LoadLevel("TutorialScene_DEMO");
+		}
+	}
+}
