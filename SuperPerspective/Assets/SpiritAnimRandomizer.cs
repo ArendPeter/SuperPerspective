767d8b0c971c21927434ed89fc1459deea43a92e
Spex130 <Gamerguy56@bellsouth.net>
2016-10-31 22:30:24 -0400

    CrystalSpirit Extra Idle Animations
    And a Randomizer script to swap between them.

---
+++ SuperPerspective/Assets/SpiritAnimRandomizer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiritAnimRandomizer : MonoBehaviour {
+
+    private Animator anim;
+    public int currentInt = 0;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void randomize()
+    {
+        currentInt = Random.Range(0, 3);
+        anim.SetInteger("RandTransition", currentInt);
+    }
+
+    public void teach()
+    {
+        anim.SetTrigger("Teach");
+    }
+
+
+}
