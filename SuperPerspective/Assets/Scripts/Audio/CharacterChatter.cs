cc968a6e52666897989ea33154fafb1630f211f7
nshooter3 <nick.shooter@gmail.com>
2016-11-01 22:16:08 -0400

    Added Character Chatter sound/prefab

---
+++ SuperPerspective/Assets/Scripts/Audio/CharacterChatter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterChatter : MonoBehaviour {
+
+    public AudioSource chatterSFX;
+    float volume, pitch, volVariation = 0.01f, pitchVariation = 0.1f;
+
+    //float timer, maxTimer = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        chatterSFX = GetComponent<AudioSource>();
+        if (chatterSFX != null)
+        {
+            volume = chatterSFX.volume;
+            pitch = chatterSFX.pitch;
+        }
+        //timer = maxTimer;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        /*if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Chatter();
+            timer = maxTimer;
+        }*/
+	}
+
+    public void Chatter()
+    {
+        chatterSFX.volume = volume + Random.Range(-volVariation, volVariation);
+        chatterSFX.pitch = pitch + Random.Range(-pitchVariation, pitchVariation);
+        chatterSFX.Play();
+    }
+
+}
