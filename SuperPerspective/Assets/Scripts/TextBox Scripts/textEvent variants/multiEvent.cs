fe70bfa55f9fe3a91b4e3be29d44401411ef4f3c
Spex130 <Gamerguy56@bellsouth.net>
2016-11-02 22:12:01 -0400

    Multi-Events Triggering script created + Properly chained Multi-Node conversation
    Now I can chain together multiple ConvoNodes so that I can have events
    happen when people are talking!

---
+++ SuperPerspective/Assets/Scripts/TextBox Scripts/textEvent variants/multiEvent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class multiEvent : textEvent {
+
+    public textEvent[] eventArray;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void eventTrigger()
+    {
+        for(int i = 0; i < eventArray.Length; i++)
+        {
+            
+            if (eventArray[i] != null)
+            {
+                print("lol");
+                eventArray[i].eventTrigger();
+            }
+}
+    }
+}
