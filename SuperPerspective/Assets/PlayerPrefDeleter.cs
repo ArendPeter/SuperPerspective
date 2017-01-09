922073afe32f842071f71eeb84f7ed77b4c55156
Spex130 <Gamerguy56@bellsouth.net>
2017-01-08 23:25:00 -0500

    Did some fixings for Textbox in the tutorial scene.
    Also made a PlayerPrefDeleter
    
    Might not be done, but pushing for posterity.

---
+++ SuperPerspective/Assets/PlayerPrefDeleter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefDeleter : MonoBehaviour {
+
+    public bool deletePrefs = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (deletePrefs)
+        {
+            PlayerPrefs.DeleteAll();
+        }
+	}
+}
