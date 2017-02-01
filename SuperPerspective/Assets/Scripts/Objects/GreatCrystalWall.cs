e3dea883e0f6ffdf1208c0044afd3899d644a533
nshooter3 <nick.shooter@gmail.com>
2017-02-01 16:24:30 -0500

    Fixed ledge grabbing bug in grass 5, more crystal wall scripts

--- SuperPerspective/Assets/Scripts/Objects/GreatCrystalWall.cs
+++ SuperPerspective/Assets/Scripts/Objects/GreatCrystalWall.cs
@@ -6,21 +6,49 @@ public class GreatCrystalWall : MonoBehaviour {
 
     public Activatable[] switches;
     bool grass = false, ice = false, desert = false, activated = false;
+    public GameObject c1, c2, c3;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
+    // Use this for initialization
+    void Start() {
+        if (PlayerPrefs.GetInt("GrassScene") == 1){
+            grass = true;
+            c1.SetActive(true);
+        }
+        if (PlayerPrefs.GetInt("DesertScene") == 1)
+        {
+            desert = true;
+            c2.SetActive(true);
+        }
+        if (PlayerPrefs.GetInt("IceScene") == 1)
+        {
+            ice = true;
+            c3.SetActive(true);
+        }
+    }
 
     public void Activate()
     {
-        if (!activated/*&&grass && ice && desert*/)
+        if (!activated)
         {
-            for (int i = 0; i < switches.Length; i++)
+            if (grass && ice && desert)
             {
-                switches[i].activated = true;
-                activated = true;
+                //Crystal Fairy closing dialogue
+                //TODO Larry, call this class's OpenGate function after the dialogue
+                OpenGate();
             }
+            else
+            {
+                //Crystal Fairy tells you to gather big crystals
+            }
+        }
+    }
+
+    public void OpenGate()
+    {
+        for (int i = 0; i < switches.Length; i++)
+        {
+            switches[i].activated = true;
+            activated = true;
         }
     }
 	
