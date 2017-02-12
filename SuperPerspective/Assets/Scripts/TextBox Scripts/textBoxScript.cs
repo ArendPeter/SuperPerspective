867b83836c98c1aaeebb27dbd54a89cd5ef32328
Spex130 <Gamerguy56@bellsouth.net>
2017-02-11 19:51:41 -0500

    Ivan Theme now fades out proper. Also, butt scoot fix.

--- SuperPerspective/Assets/Scripts/TextBox Scripts/textBoxScript.cs
+++ SuperPerspective/Assets/Scripts/TextBox Scripts/textBoxScript.cs
@@ -164,7 +164,7 @@ public class textBoxScript : MonoBehaviour {
         //textSpeed = resetTextSpeed;
         showBox = false;
 		disableBox();
-        //uim.StopCrystalFairyTheme();
+        uim.StopCrystalFairyTheme();
 	}
 	public void enableBox()//This enables the textbox so you can see it.
 	{
