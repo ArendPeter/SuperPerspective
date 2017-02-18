a7701af79ab2ac69d5b34a96c3687dd245bd5634
Spex130 <Gamerguy56@bellsouth.net>
2017-02-17 20:28:02 -0500

    Ivan now doesn't show up in the hub till you talk to him.

---
+++ SuperPerspective/Assets/Scripts/TextBox Scripts/activateIvanEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateIvanEvent : MonoBehaviour
+{ 
+    public FairyFollow ff;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlayerPrefs.GetString("IntroCutsceneFinished") == "true")
+        {
+            ff.shouldFollow = true;
+        }
+    }
+
+}
