ac4d7201dbf932248accccdf6d967d7dd27d1a68
nshooter3 <nick.shooter@gmail.com>
2017-05-13 21:44:45 -0400

    Commented out debug foreach that was causing warp to fire multiple times

--- SuperPerspective/Assets/Scripts/Objects/Bonfire.cs
+++ SuperPerspective/Assets/Scripts/Objects/Bonfire.cs
@@ -32,7 +32,7 @@ public class Bonfire : ActiveInteractable {
 	void Update() {
 		if (active) {
 			if (bonfireUI.teleportFlag || bonfireUI.closeFlag) {
-				foreach (string s in foundDoors)
+				//foreach (string s in foundDoors)
 					//Debug.Log(s);
 				if (bonfireUI.teleportFlag && foundDoors[bonfireUI.choice] != "")
 					Door.TeleportPlayerToDoor(player.GetComponent<PlayerController>(), foundDoors[bonfireUI.choice]);
