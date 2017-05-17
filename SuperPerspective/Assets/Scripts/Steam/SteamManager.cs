2181dfc3ee9b8ba19c7def0f0b60996eefa7dbde
Arend Peter Castelein <apc1993@gmail.com>
2017-05-16 21:26:15 -0400

    Implemented facepunch plugin

--- SuperPerspective/Assets/Scripts/Steam/SteamManager.cs
+++ SuperPerspective/Assets/Scripts/Steam/SteamManager.cs
@@ -7,174 +7,75 @@
 
 using UnityEngine;
 using System.Collections;
-using Steamworks;
+using Facepunch.Steamworks;
 
-//
-// The SteamManager provides a base implementation of Steamworks.NET on which you can build upon.
-// It handles the basics of starting up and shutting down the SteamAPI for use.
-//
-[DisallowMultipleComponent]
 public class SteamManager : MonoBehaviour {
-	private static SteamManager s_instance;
-	private static SteamManager Instance {
-		get {
-			if (s_instance == null) {
-				return new GameObject("SteamManager").AddComponent<SteamManager>();
-			}
-			else {
-				return s_instance;
-			}
-		}
-	}
+		public static Facepunch.Steamworks.Client SteamClient;
+    private ServerList.Request serverRequest;
 
-	private static bool s_EverInialized;
+    void Start (){
+		    //
+        // Configure for Unity
+        //
+		    Facepunch.Steamworks.Config.ForUnity( Application.platform.ToString() );
 
-	private bool m_bInitialized;
-	public static bool Initialized {
-		get {
-			return Instance.m_bInitialized;
-		}
-	}
+        //
+        // Create the steam client using Rust's AppId
+        //
+        SteamClient = new Client( 629420 );
 
-	private SteamAPIWarningMessageHook_t m_SteamAPIWarningMessageHook;
-	private static void SteamAPIDebugTextHook(int nSeverity, System.Text.StringBuilder pchDebugText) {
-		Debug.LogWarning(pchDebugText);
-	}
+        //
+        // Make sure we started up okay
+        //
+        if ( !SteamClient.IsValid )
+        {
+            SteamClient.Dispose();
+            SteamClient = null;
+            return;
+        }
 
-	private void Awake() {
-		// Only one instance of SteamManager at a time!
-		if (s_instance != null) {
-			Destroy(gameObject);
-			return;
-		}
-		s_instance = this;
+        //
+        // Request a list of servers
+        //
+		    {
+	        serverRequest = SteamClient.ServerList.Internet();
+		    }
 
-		if(s_EverInialized) {
-			// This is almost always an error.
-			// The most common case where this happens is when SteamManager gets destroyed because of Application.Quit(),
-			// and then some Steamworks code in some other OnDestroy gets called afterwards, creating a new SteamManager.
-			// You should never call Steamworks functions in OnDestroy, always prefer OnDisable if possible.
-			throw new System.Exception("Tried to Initialize the SteamAPI twice in one session!");
-		}
+        var gotStats = false;
+        SteamClient.Achievements.OnUpdated += () => { gotStats = true; };
 
-		// We want our SteamManager Instance to persist across scenes.
-		DontDestroyOnLoad(gameObject);
+        while ( !gotStats )
+        {
+            SteamClient.Update();
+        }
 
-		if (!Packsize.Test()) {
-			Debug.LogError("[Steamworks.NET] Packsize Test returned false, the wrong version of Steamworks.NET is being run in this platform.", this);
+				testFunction();
 		}
 
-		if (!DllCheck.Test()) {
-			Debug.LogError("[Steamworks.NET] DllCheck Test returned false, One or more of the Steamworks binaries seems to be the wrong version.", this);
-		}
-
-		try {
-			// If Steam is not running or the game wasn't started through Steam, SteamAPI_RestartAppIfNecessary starts the
-			// Steam client and also launches this game again if the User owns it. This can act as a rudimentary form of DRM.
-
-			// Once you get a Steam AppID assigned by Valve, you need to replace AppId_t.Invalid with it and
-			// remove steam_appid.txt from the game depot. eg: "(AppId_t)480" or "new AppId_t(480)".
-			// See the Valve documentation for more information: https://partner.steamgames.com/documentation/drm#FAQ
-			if (SteamAPI.RestartAppIfNecessary(AppId_t.Invalid)) {
-				Application.Quit();
-				return;
-			}
-		}
-		catch (System.DllNotFoundException e) { // We catch this exception here, as it will be the first occurence of it.
-			Debug.LogError("[Steamworks.NET] Could not load [lib]steam_api.dll/so/dylib. It's likely not in the correct location. Refer to the README for more details.\n" + e, this);
-
-			Application.Quit();
-			return;
-		}
-
-		// Initialize the SteamAPI, if Init() returns false this can happen for many reasons.
-		// Some examples include:
-		// Steam Client is not running.
-		// Launching from outside of steam without a steam_appid.txt file in place.
-		// Running under a different OS User or Access level (for example running "as administrator")
-		// Ensure that you own a license for the AppId on your active Steam account
-		// If your AppId is not completely set up. Either in Release State: Unavailable, or if it's missing default packages.
-		// Valve's documentation for this is located here:
-		// https://partner.steamgames.com/documentation/getting_started
-		// https://partner.steamgames.com/documentation/example // Under: Common Build Problems
-		// https://partner.steamgames.com/documentation/bootstrap_stats // At the very bottom
-
-		// If you're running into Init issues try running DbgView prior to launching to get the internal output from Steam.
-		// http://technet.microsoft.com/en-us/sysinternals/bb896647.aspx
-		m_bInitialized = SteamAPI.Init();
-		if (!m_bInitialized) {
-			Debug.LogError("[Steamworks.NET] SteamAPI_Init() failed. Refer to Valve's documentation or the comment above this line for more information.", this);
-
-			return;
-		}
-
-		s_EverInialized = true;
-
-		testFunction();
-	}
-
-	// This should only ever get called on first load and after an Assembly reload, You should never Disable the Steamworks Manager yourself.
-	private void OnEnable() {
-		if (s_instance == null) {
-			s_instance = this;
-		}
-
-		if (!m_bInitialized) {
-			return;
-		}
-
-		if (m_SteamAPIWarningMessageHook == null) {
-			// Set up our callback to recieve warning messages from Steam.
-			// You must launch with "-debug_steamapi" in the launch args to recieve warnings.
-			m_SteamAPIWarningMessageHook = new SteamAPIWarningMessageHook_t(SteamAPIDebugTextHook);
-			SteamClient.SetWarningMessageHook(m_SteamAPIWarningMessageHook);
-		}
-	}
+		void Update()
+    {
+        if ( SteamClient != null )
+        {
+            SteamClient.Update();
 
 
-	// OnApplicationQuit gets called too early to shutdown the SteamAPI.
-	// Because the SteamManager should be persistent and never disabled or destroyed we can shutdown the SteamAPI here.
-	// Thus it is not recommended to perform any Steamworks work in other OnDestroy functions as the order of execution can not be garenteed upon Shutdown. Prefer OnDisable().
-	private void OnDestroy() {
-		if (s_instance != this) {
-			return;
-		}
-
-		s_instance = null;
-
-		if (!m_bInitialized) {
-			return;
-		}
-
-		SteamAPI.Shutdown();
-	}
-
-	private void Update() {
-		if (!m_bInitialized) {
-			return;
-		}
-
-		// Run Steam client callbacks
-		SteamAPI.RunCallbacks();
-	}
+        }
+    }
 
 	private void testFunction(){
 		Debug.Log(GetSteamName());
 		print("all desert crystals?");
-		print(GetAchievement("a1"));
+		print(GetAchievement("a3"));
 		print("all crystals in game?");
 		print(GetAchievement("a2"));
 		print("setting all desert crystals to true");
-		AchieveAllCrystalsInDesert();
+		AchieveAllCrystalsInGrass();
 		print("all desert crystals?");
-		print(GetAchievement("a1"));
+		print(GetAchievement("a3"));
 	}
 
 	public string GetSteamName(){
-		if(SteamManager.Initialized){
-			return SteamFriends.GetPersonaName();
-		}
-		return "";
+		return SteamClient.Username;
 	}
 
 	public bool CheckAllCrystalsInDesert(){
@@ -242,14 +143,19 @@ public class SteamManager : MonoBehaviour {
 	}
 
 	public void SetAchievement(string id){
-		if(SteamManager.Initialized){
-			 SteamUserStats.SetAchievement(id);
-		}
+			foreach ( var ach in SteamClient.Achievements.All ){
+          if(ach.Id == id){
+            ach.Trigger();
+          }
+      }
 	}
 
 	public bool GetAchievement(string id){
-			bool check_it;
-			bool res = SteamUserStats.GetAchievement(id,out check_it);
-			return res;
+			foreach ( var ach in SteamClient.Achievements.All ){
+          if(ach.Id == id){
+							return ach.State;
+          }
+      }
+			return false;
 	}
 }
